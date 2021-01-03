4ef6351b2d5702dc943497946eca294f30f28bbe
Fernando Torres <ftorresm.7@gmail.com>
2021-01-02 21:38:29 -0300

    added /twig

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,13 +2,19 @@
 // src/Controller/LuckyController.php
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
+use Symfony\Component\HttpFoundation\Response;
 
 class TestController extends AbstractController
 {
-    public function hello(): Response
+    public function hello()
+    {
+        return new Response(
+            '<html><body>Hello Synolia</body></html>'
+        );
+    }
+
+    public function twig(): Response
     {
         $message = 'Twig is a modern template engine for PHP';
 
